c59c4247ac91c50ef295ce679444fecc778a9997
unknown <denisenmarcelo@gmail.com>
2023-05-02 13:15:04 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1.5rem',
+      screens: {
+        '2xl': '1360px'
+      },
+    },
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
